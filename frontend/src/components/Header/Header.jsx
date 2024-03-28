6a2a2888c1cfc8567cfe7e49ca0283f9319ab838
Rohit Mishra <rohitaniket09@gmail.com>
2024-03-28 16:45:28 +0530

    Home page changes
    Co-Authored-By: prakhar chauhan <88184122+prakharjuit@users.noreply.github.com>

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import whatsapp from '../../pictures/whatsapp.png'
 import image2 from '../../pictures/image2.png'
+
+import button1 from '../../pictures/button1.png'
+import button2 from '../../pictures/button2.png'
+
 import './Header.css'
 const Header = () => {
   return (
@@ -17,14 +21,20 @@ const Header = () => {
         <h1>Empower Your Expertise!</h1>
         <p>“Start, switch or elevate your career with   our Comprehensive Scaled Agile Methodology Courses”</p>
         <div>
-        <button className='image-button1'></button>
-        <button className='image-button2'></button>
-       <img className="whatsapp" src={whatsapp} alt="WhatsApp Logo" />
+        {/* <button className='image-button1'>{button1}</button>
+        <button className='image-button2'></button> */}
+        <div className='container'>
+        <button className='myButton' style={{paddingLeft:"28px"}}>
+          View Courses
+        </button>
+        <button className='myButton'>
+          Request a Demo
+        </button>
+        </div>
+        
+        <img className="whatsapp" src={whatsapp} alt="WhatsApp Logo" />
        </div>
         </div>
-      
-
-
       </div>
     </>
   )
